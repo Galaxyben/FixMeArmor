e5c29f079651d1834bd9e0ebf41c1480e1db3bb1
Retus001 <retus001@gmail.com>
2020-02-01 13:17:07 -0600

    Hammer update

---
+++ Assets/_main/Hammering/Scripts/HammerBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerBehaviour : MonoBehaviour
+{
+    [Header("Hammer")]
+    public string useHammerInput;
+    public string useHammerAnimTrigger;
+
+    private Animator anim;
+    private GameObject target;
+
+    // Inputs
+    private bool useHammer;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void GetInputs()
+    {
+        if (Input.GetButtonDown(useHammerInput))
+        {
+            useHammer = true;
+        } else
+        {
+            useHammer = false;
+        }
+    }
+
+    public void ProcessInputs()
+    {
+        if (useHammer) { Use(); }
+    }
+
+    public void Use()
+    {
+        anim.SetTrigger(useHammerAnimTrigger);
+    }
+}

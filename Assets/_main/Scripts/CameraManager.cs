722fa161173842b09a932de1ca7e78bd74c064da
Galaxyben <galaxyben1@gmail.com>
2020-02-01 21:05:56 -0600

    CameraManager

---
+++ Assets/_main/Scripts/CameraManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CameraManager : MonoBehaviour
+{
+    public PostProcessVolume postProcess;
+    float depthOfFieldInitial = 1.1f;
+    float depthOfFieldStandard = 15f;
+    DepthOfField DoF;
+
+    public Transform hammeringCamPos;
+    public Transform solderingCamPos;
+    public Transform bakingCamPos;
+    
+    void Start()
+    {
+        postProcess.profile.TryGetSettings(out DoF);
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void SetToHamering()
+    {
+        Camera.main.transform.position = hammeringCamPos.position;
+        Camera.main.transform.rotation = hammeringCamPos.rotation;
+    }
+
+    public void SetToSoldering()
+    {
+        Camera.main.transform.position = solderingCamPos.position;
+        Camera.main.transform.rotation = solderingCamPos.rotation;
+    }
+
+    public void SetToBake()
+    {
+        Camera.main.transform.position = bakingCamPos.position;
+        Camera.main.transform.rotation = bakingCamPos.rotation;
+    }
+
+    public void DoF_Standard()
+    {
+        DoF.focusDistance.value = depthOfFieldStandard;
+    }
+
+    public void DoF_Blur()
+    {
+        DoF.focusDistance.value = depthOfFieldInitial;
+    }
+}
---
+++ Assets/_main/Scripts/MasterManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterManager : MonoBehaviour
+{
+    CameraManager cm;
+
+    void Start()
+    {
+        cm = GetComponent<CameraManager>();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void StartPlaying()
+    {
+
+    }
+}
